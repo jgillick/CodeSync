860565a447b3ebcfc7fb123420f5fe06951be5b2
Lisa Manresa <lmanresa@linkedin.com>
2012-08-22 12:57:05 -0700

    Adding the rb-fsevent library to remove dependency on rubycocoa and allow codesync to work on OS X Lion.

--- lib/fsevents/ext.rb
+++
@@ -1,1 +0,0 @@
-require 'fsevents/ext/array'
--- lib/fsevents/version.rb
+++
@@ -1,9 +0,0 @@
-module Fsevents #:nodoc:
-  module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 1
-    TINY  = 1
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-end
